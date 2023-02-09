d9eea4a8e908950bda6a60c29230b6c8e61ee0dd
shopgrok <shopgrok@shopgroks-MacBook-Air.local>
2023-02-09 20:13:35 +1100

    create all templates file required and function views for comments and post publish

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,13 @@ from blog import views
 urlpatterns = [
     path('',view.PostListView.as_view(),name='post_list'),
     path('about',views.AboutView.as_view(),name='about'),
-    path('post/<pk>',view.PostDetailView.as_view(),name='post_detail'),
-    path('post/new',view.PostCreateView.as_view(),name='post_new'),
-    path('post/<pk>/edit',view.PostUpdateView.as_view(),name='post_edit'),
-    path('post/<pk>/remove',view.PostDeleteView.as_view(),name='post_remove'),
-    path('drafts',view.DraftListView.as_view(),name='post_draft_list')
+    path('post/<pk>',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new',views.PostCreateView.as_view(),name='post_new'),
+    path('post/<pk>/edit',views.PostUpdateView.as_view(),name='post_edit'),
+    path('post/<pk>/remove',views.PostDeleteView.as_view(),name='post_remove'),
+    path('drafts',views.DraftListView.as_view(),name='post_draft_list'),
+    path('post/<pk>/comment',views.add_comment_to_post,name='add_comment_to_post'),
+    path('comment/<pk>/approve',views.comment_approve,name='comment_approve'),
+    path('comment/<pk>/remove',views.comment_remove,name='comment_remove'),
+    path('post/<pk>/publish',views.post_publish,name='post_publish')
 ]
